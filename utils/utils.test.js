f748e9f91eb67c6b84b46591cb3f8953db8e6ef3
YoProgrammer <yuttasakcom@gmail.com>
2017-11-19 11:36:21 +0700

    add expect test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,10 +1,23 @@
-const { add } = require('./utils')
+const expect = require('expect')
+const { add, square } = require('./utils')
 
 it('should add two numbers', () => {
-  let result = add(1, 2)
+  let res = add(1, 2)
 
-  if (result !== 3) {
-    throw new Error(`Expected 3, but got ${result}`)
+  if (res !== 3) {
+    throw new Error(`Expected 3, but got ${res}`)
   }
-  
+})
+
+it('should add two numbers', () => {
+  let res = add(1, 2)
+  expect(res).toBe(3)
+})
+
+it('should square a number', () => {
+  let res = square(4)
+
+  if (res !== 16) {
+    throw new Error(`Expected 16, but got ${res}`)
+  }
 })
